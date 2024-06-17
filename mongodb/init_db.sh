b1748c4675234d0cc0d3cfc9a0784bc2c96a20c1
Ana Vekua <annavekua@live.be>
2024-06-17 15:08:27 +0400

    remove unnecessary spaces

--- mongodb/init_db.sh
+++ mongodb/init_db.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source /data/db/.env
 mongosh <<EOF
-
 use $MONGO_INITDB_DATABASE
 db.createUser(
         {
@@ -15,13 +14,10 @@ db.createUser(
             ]
         }
 );
-
-
 db.createCollection('election');
 db.createCollection('usa_election_articles');
 db.createCollection('book');
 db.createCollection('polarity_dataset');
-
 EOF
 mongoimport -d $MONGO_INITDB_DATABASE -c election --authenticationDatabase $MONGO_INITDB_DATABASE --username $USER1 --password $PASSWORD1 --type json --file data/db/data/election.json --jsonArray
 mongoimport -d $MONGO_INITDB_DATABASE -c polarity_dataset --type csv --authenticationDatabase $MONGO_INITDB_DATABASE --username $USER1 --password $PASSWORD1 --file data/db/data/merge_SEN.csv --headerline
