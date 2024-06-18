5d5816a9ae4c25600c09d5b92ab23bbab46a9e4a
Sarahdni <168664836+Sarahdni@users.noreply.github.com>
2024-06-18 13:29:55 +0200

    Update nyt_article_fetcher.py
    Ajout de la méthode parse_date

--- nyt_api_archive/fetcher_service/nyt_article_fetcher.py
+++ nyt_api_archive/fetcher_service/nyt_article_fetcher.py
@@ -21,11 +21,27 @@ class NYTArticleFetcher:
         data = response.json()
         return data.get('response', {}).get('docs', [])
 
+     @staticmethod
+     def parse_date(date_str):
+        # Liste des formats de date possibles
+        date_formats = [
+            '%Y-%m-%dT%H:%M:%S%z',
+            '%Y-%m-%dT%H:%M:%S.%f%z'
+        ]
+        for fmtt in date_formats:
+            try:
+                return datetime.strptime(date_str, fmt)
+            except ValueError:
+                continue
+        raise ValueError(f"Date format for {date_str} not recognized")
+
     @staticmethod
-    def extract_fields(article: Dict[str, Any]) -> Dict[str, Any]:
+    def extract_fields(article):
         """
         Extrait les clés-valeurs déterminées au préalable.
         """
+        pub_date_str = article.get("pub_date", "1970-01-01T00:00:00+0000")
+        pub_date = NYTArticleFetcher.parse_date(pub_date_str).strftime('%Y-%m-%d %H:%M:%S')
         return {
             "abstract": article.get("abstract", ""),
             "headline": article.get("headline", {}).get("main", ""),
