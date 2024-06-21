ad3419154686c23808e0e2717fe719669e40ed5b
Ana Vekua <annavekua@live.be>
2024-06-21 15:06:40 +0400

    add importation nyt articles archive while the ingestion is not automated

--- mongodb/init_db.sh
+++ mongodb/init_db.sh
@@ -22,3 +22,4 @@ EOF
 mongoimport -d $MONGO_INITDB_DATABASE -c election --authenticationDatabase $MONGO_INITDB_DATABASE --username $USER1 --password $PASSWORD1 --type json --file data/db/data/election.json --jsonArray
 mongoimport -d $MONGO_INITDB_DATABASE -c polarity_dataset --type csv --authenticationDatabase $MONGO_INITDB_DATABASE --username $USER1 --password $PASSWORD1 --file data/db/data/merge_SEN.csv --headerline
 mongoimport -d $MONGO_INITDB_DATABASE -c book --authenticationDatabase $MONGO_INITDB_DATABASE --username $USER1 --password $PASSWORD1 --type json --file data/db/data/bestsellers_with_abstract_and_genre_3350.json --jsonArray
+#mongoimport -d $MONGO_INITDB_DATABASE -c usa_election_articles --authenticationDatabase $MONGO_INITDB_DATABASE --username $USER1 --password $PASSWORD1 --type json --file data/db/data/nytimes_archives_elections_articles.json --jsonArray
