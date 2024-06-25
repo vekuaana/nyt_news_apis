16b7a25de6edb2e4642998e03a478a0002828646
Asceline Goudjo <asc.amara@gmail.com>
2024-06-26 00:54:16 +0200

    test

--- ml/predict/fake.py
+++ ml/predict/fake.py
@@ -5,3 +5,8 @@ def faker():
     with open('..' + os.sep + 'models' + os.sep + 'mytest2.txt') as f:
         text = f.read()
     return text
+
+
+if __name__=="__main__":
+    while True:
+        faker()
