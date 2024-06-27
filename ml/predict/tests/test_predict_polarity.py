a610b87062075ea067f747c35a4fccc9b0cafd22
Asceline Goudjo <asc.amara@gmail.com>
2024-06-27 04:59:52 +0200

    test

--- ml/predict/tests/test_predict_polarity.py
+++ ml/predict/tests/test_predict_polarity.py
@@ -4,7 +4,6 @@ from predict_polarity import Polarity
 
 def test_predict():
     text = "Biden Goes After Trump’s Felon Status at Connecticut Fund-Raiser"
-    print(os.getcwd())
     get_polarity = Polarity(model_dir=os.getcwd()+ os.sep + '..' + os.sep + "models")
     res = get_polarity.predict(text, "2024")
     assert [{'entity': 'Biden', 'prediction': 'neutral'}, {'entity': 'Trump', 'prediction': 'negative'}] == res
